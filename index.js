a96b724d003872d005cd407447c8b4593e1487b9
Mack Predelus <macksbook@Macks-MBP.attlocal.net>
2024-05-29 00:21:01 -0700

    completed responses

--- index.js
+++ index.js
@@ -7,27 +7,62 @@ const questions = [ // creates an array of questions to select in command line
         type: "input",
         name: "text",
         message: "Choose 3 letters to display"
-        // TODO -->add choice hexidecimal number or color keyword 
+
 
     },
     {
         type: "input",
         name: "textColor",
         message: "What color will the text in the shape be?"
-                // TODO -->add choice hexidecimal number or color keyword 
-        
+        // TODO -->add choice hexidecimal number or color keyword 
+
     },
     {
         type: "list",
         name: "shape",
-        message: "Choose a shape.",
+        message: "Choose a shape between: circle, square, and triangle.",
         choices: ["circle", "square", "triangle"]
 
     },
     {
-        type: "",
-        name: "",
-        message: ""
+        type: "input",
+        name: "shapeColor",
+        message: "What color will the shape be?"
+        // TODO -->add choice hexidecimal number or color keyword 
 
     }
-]
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err, result) => {
+        err ? console.error(err) : console.log('Logo has been created.');;
+    })
+}
+
+function generateSVG({ text, textColor, shape, shapeColor }) {
+    let shapeInstance;
+    switch (shape) {
+        case 'circle':
+            shapeInstance = new Circle(text, textColor, shapeColor);
+            break;
+        case 'square':
+            shapeInstance = new Square(text, textColor, shapeColor);
+            break;
+        case 'triangle' = new Triangle(text, textColor, shapeColor);
+            break;
+        default:
+            throw new Error('Invalid shape type')
+    }
+    return shapeInstance.render();    
+    
+}
+
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            const svgContent = generateSVG(response);
+            writeToFile('logo.svg', svgContent);
+        });
+}
+
+init();
